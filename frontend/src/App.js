126bc669fae595237b7322fce02b3fba4fa8a120
Evan Reid <evan@reids.us>
2020-03-03 18:31:37 -0800

    Add initial login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,15 +5,30 @@ import { connect, sendMsg } from './api';
 import Header from './components/Header/Header';
 import ChatHistory from './components/ChatHistory/ChatHistory'
 import ChatInput from './components/ChatInput';
+import LoginForm from './components/Login/LoginForm';
 
 class App extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
+      isActive: false,
+      user: null,
       chatHistory: []
     }
   }
+
+  handleShow = () => {
+    this.setState({
+      isActive: true
+    });
+  };
+
+  handleHide = () => {
+    this.setState({
+      isActive: false
+    });
+  };
 
   componentDidMount() {
     connect((msg) => {
@@ -32,12 +47,39 @@ class App extends Component {
     }
   }
 
+  // Login Methods
+  signIn(username, password) {
+    // add auth here
+    this.setState({
+      user: {
+        username,
+        password
+      }
+    })
+  }
+
+  signOut() {
+    // clear user from state
+    this.setState({
+      user: null
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <Header />
-        <ChatHistory chatHistory={this.state.chatHistory} />
-        <ChatInput send={this.send} />
+      <Header />
+      {  
+          (this.state.user) ?
+            <div>
+              <ChatHistory chatHistory={this.state.chatHistory} />
+              <ChatInput send={this.send} />
+            </div>
+          :
+            <LoginForm
+              onSignIn={this.signIn.bind(this)}
+            />
+      }      
       </div>
     );
   }
